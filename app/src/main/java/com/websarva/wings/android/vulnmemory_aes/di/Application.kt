5aa20eb6ae224bb1f8a6f0227e4302b6e1080812
daiki0508 <oochani05085@outlook.jp>
2021-11-10 13:37:19 +0900

    feat: Implemented the process of encrypting messages with AES. In addition, the process of displaying the encrypted message on the screen is also implemented.

---
+++ app/src/main/java/com/websarva/wings/android/vulnmemory_aes/di/Application.kt
@@ -0,0 +1,8 @@
+package com.websarva.wings.android.vulnmemory_aes.di
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class Application: Application() {
+}
--- app/src/main/java/com/websarva/wings/android/vulnmemory_aes/ui/fragment/AESNativeFragment.kt
+++ app/src/main/java/com/websarva/wings/android/vulnmemory_aes/ui/fragment/AESNativeFragment.kt
@@ -1,16 +1,23 @@
 package com.websarva.wings.android.vulnmemory_aes.ui.fragment
 
 import android.os.Bundle
+import android.text.Editable
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.viewModels
 import com.websarva.wings.android.vulnmemory_aes.databinding.FragmentAesnativeBinding
+import com.websarva.wings.android.vulnmemory_aes.viewmodel.aesnative.AESNativeViewModel
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class AESNativeFragment: Fragment() {
     private var _binding: FragmentAesnativeBinding? = null
     private val binding
     get() = _binding!!
+
+    private val viewModel: AESNativeViewModel by viewModels()
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -24,5 +31,18 @@ class AESNativeFragment: Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+
+        // ボタンタップ時の処理
+        binding.button.setOnClickListener {
+            if (!binding.edText.text.isNullOrBlank())
+                viewModel.encrypt(binding)
+        }
+
+        // ivのobserver
+        with(viewModel){
+            flag.observe(this@AESNativeFragment.viewLifecycleOwner, {
+                binding.tvResult.text = "encrypted: $encryptData\niv: $iv"
+            })
+        }
     }
 }
